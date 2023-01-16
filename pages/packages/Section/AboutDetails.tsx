27466a616c492605ceb324d61ba8b512b6deac64
Stefan <stefa.vuksan.9881@gmail.com>
2023-01-16 09:59:16 -0800

    add animations to about details section

--- pages/packages/Section/AboutDetails.tsx
+++ pages/packages/Section/AboutDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
 import { ImageLoader } from '@movit/utils'
 
@@ -6,6 +6,10 @@ import { Card, Col, Container, Row, Stack } from 'react-bootstrap'
 import Section from '../Section'
 
 import TickIcon from '@movit/image/aboutus/tick.svg'
+
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const ListItem = ({label} : {label: string}) =>
   <Stack direction='horizontal' gap={3} className='align-items-start'>
@@ -24,20 +28,79 @@ const SectionAboutDetails = () => {
     "Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem.",
     "Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem.",
   ]
+
+  const lines = useRef<any>([]);
+  const images = useRef<any>([]);
+
+  useEffect(() => {
+    gsap.fromTo(lines.current,
+      {
+        autoAlpha: 0,
+        y: +100,
+      },
+      {
+        duration: .7, 
+        autoAlpha: 1,
+        y: 0,
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: lines.current[0],
+          start: 'top bottom-=100',
+          toggleActions: 'play none none reverse',
+        }
+      }  
+    );
+    
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: images.current[0],
+        start: 'top bottom-=100',
+        toggleActions: 'play none none reverse',
+      }
+    }).fromTo(images.current[0],
+      {
+        x: '-100%',
+      },
+      {
+        x: 0,
+        onStart: ()=>{
+          gsap.set(images.current[1], {opacity: 0});
+        },
+        onComplete: ()=>{
+          gsap.set(images.current[1], {opacity: 1});
+        }
+      }
+    ).fromTo(images.current[0],
+      {
+        x: 0,
+      },
+      {
+        x: '100%',
+      }
+    ).play();
+
+  }, [])
+
+  const addToRefs = (el: any, list: any[]) => {
+    if (el && !list.includes(el)) {
+        list.push(el);
+    }
+  };
   return (
     <Section name="about-details">
       <Container>
         <Row xs={1} xxl={2}>
           <Col>
             <Stack>
-              <h2 className='col-xxxl-10'>Wie kann movIT Deinem Unternehmen in bewegung</h2>
-              <h6 className='col-10 col-xxxl-9 py-4'>Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem.</h6>
-              {lists.map((item, idx)=><ListItem key={idx} label={item}/>)}
+              <h2 ref={el=>addToRefs(el, lines.current)} className='col-xxxl-10'>Wie kann movIT Deinem Unternehmen in bewegung</h2>
+              <h6 ref={el=>addToRefs(el, lines.current)} className='col-10 col-xxxl-9 py-4'>Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem. Erhöhen Sie Ihre Umsätze durch ein immer verfügbares Online-Terminbuchungssystem.</h6>
+              <div ref={el=>addToRefs(el, lines.current)}>{lists.map((item, idx)=><ListItem key={idx} label={item}/>)}</div>
             </Stack>
           </Col>
           <Col>
-            <Card>
-              <Card.Img src='/image/aboutus/details.png' />
+            <Card className='overflow-hidden'>
+              <div ref={el=>addToRefs(el, images.current)} className='position-absolute bg-gray-200 w-100 h-100'></div>
+              <Card.Img ref={(el:any)=>addToRefs(el, images.current)} src='/image/aboutus/details.png' />
             </Card>
           </Col>
         </Row>
