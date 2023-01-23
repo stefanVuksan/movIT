d0667149980f895d1773e59ea464fb7db6e2167b
Stefan <stefa.vuksan.9881@gmail.com>
2023-01-23 03:45:54 -0800

    dropdown navbar

--- pages/packages/Component/Navbar/Menu.tsx
+++ pages/packages/Component/Navbar/Menu.tsx
@@ -1,28 +1,119 @@
-import React from 'react'
+import React, { createContext, useContext, useState, MouseEvent, createRef, useEffect } from 'react'
 
 import { Nav,  NavDropdown} from "react-bootstrap";
 
-const MenuDesktop = () => {
-  return (
-      <Nav
-          className="me-auto w-100 justify-content-center align-items-center gap-xxl-5"
-          navbarScroll
-      >
-         <Nav.Link href="#action1">PRODUCTS</Nav.Link>
-         <Nav.Link href="#action2">BUSINESS TYPES</Nav.Link>
-         <Nav.Link href="#action3">THE MOVEMENT</Nav.Link>
-         <Nav.Link href="#action4">PRICING</Nav.Link>
-         {/* <NavDropdown className='navLinks' title="Lorem Ipsum" >
-             <NavDropdown.Item className='navLinks' href="#action5">Lorem Ipsum</NavDropdown.Item>
-             <NavDropdown.Item className='navLinks' href="#action6">
-                 Lorem Ipsum
-             </NavDropdown.Item>
-             <NavDropdown.Divider />
-             <NavDropdown.Item className='navLinks' href="#action7">
-                 Lorem Ipsum
-             </NavDropdown.Item>
-         </NavDropdown> */}
+type menuContextType = {
+  height: string,
+  mobileBreak: Number,
+  setHeight: (h: string) => void,
+};
 
+const menuContextDefaultValue: menuContextType = {
+  height: 'auto',
+  mobileBreak: 768,
+  setHeight: (h: string) => {}
+}
+const MenuContext = createContext<menuContextType>(menuContextDefaultValue);
+
+const NavItemRoot = (props: any) => {
+  const [show, setShow] = useState<Boolean>(false);
+  const containerElm = createRef<HTMLDivElement>();
+  const [height, setHeight] = useState<string>('auto');
+  const mobileBreak = 768;
+
+  useEffect(() => {
+    if ( window.innerWidth > mobileBreak ) {
+      const rect = containerElm.current?.getBoundingClientRect();
+      if ( rect ) setHeight(rect.height + 'px');
+    }
+  }, [show])
+
+  const onMouseOver = () => {
+    if ( window.innerWidth > mobileBreak ) setShow(true);
+  }
+
+  const onMouseLeave = () => {
+    if ( window.innerWidth > mobileBreak ) setShow(false);
+  }
+
+  const onClick = () => {
+    if ( window.innerWidth < mobileBreak ) {
+      setShow(!show);
+    }
+  }
+
+  return (
+    props.children ? 
+    <MenuContext.Provider value={{height, mobileBreak, setHeight}}>
+      <Nav.Item onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
+        <Nav.Link onClick={onClick} className={`cnavlink d-flex flex-column ${show ? 'active' : ''}`}><span className='mtitle haschild text-uppercase'>{props.title}</span></Nav.Link>
+        {show && <Nav.Item ref={containerElm} className='cnavroot' style={{height}}>{props.children}</Nav.Item>}
+      </Nav.Item>
+    </MenuContext.Provider>
+    :
+    <Nav.Link className='text-uppercase'>{props.title}</Nav.Link>
+  )
+}
+
+const NavItem = (props: any) => {
+  const [show, setShow] = useState<Boolean>(false);
+  const [height, setHeight] = useState<string>('auto');
+  const containerElm = createRef<HTMLDivElement>();
+  const menuContext = useContext(MenuContext);
+
+  useEffect(() => {
+    const rect = containerElm.current?.getBoundingClientRect();
+    const menuHeight = parseFloat(menuContext.height);
+    if ( show ) {
+      if ( rect )
+      {
+        if ( menuHeight < rect.height )
+          menuContext.setHeight(rect.height + 'px');
+        else
+          setHeight(menuHeight + 'px');
+      }
+    }
+  }, [show])
+
+  const onMouseOver = () => {
+    if ( window.innerWidth > menuContext.mobileBreak ) setShow(true);
+  }
+
+  const onMouseLeave = () => {
+    if ( window.innerWidth > menuContext.mobileBreak ) setShow(false);
+  }
+
+  const onClick = () => {
+    if ( window.innerWidth < menuContext.mobileBreak ) {
+      setShow(!show);
+    }
+  }
+
+  return (
+    props.children ?
+    <Nav.Item onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>
+      <Nav.Link onClick={onClick} className={`cnavlink d-flex flex-column ${show ? 'active' : ''}`}><span className='title haschild'>{props.title}</span><span className='desc'>{props.description}</span></Nav.Link>
+      {show && <Nav.Item ref={containerElm} className='cnavsub' style={{height}}>{props.children}</Nav.Item>}
+    </Nav.Item>
+    :
+    <Nav.Link onMouseOver={()=>setShow(true)} onMouseLeave={()=>setShow(false)} className={`cnavlink d-flex flex-column ${show ? 'active' : ''}`}><span className='title'>{props.title}</span><span className='desc'>{props.description}</span></Nav.Link>
+  )
+}
+
+const MenuItemRecursive = (props: any) => {
+  return props.item && <NavItem title={props.item.title} description={props.item.description}>
+    {props.item.children?.map((child: any, idx: Number) => <MenuItemRecursive key={idx} item={child} />)}
+  </NavItem>
+}
+
+const MenuDesktop = (props: any) => {
+  return (
+      <Nav className="me-auto w-100 justify-content-md-center align-items-md-center gap-xxl-5">
+        {
+          props.menus?.map((menu: any, idx: Number) => <NavItemRoot key={idx} title={menu.title}>
+            {menu.children?.map((child: any, i: Number) => <MenuItemRecursive key={i} item={child} />)}
+          </NavItemRoot>)
+        }
       </Nav>
   )
 }
--- pages/packages/Component/Navbar/NavBar.tsx
+++ pages/packages/Component/Navbar/NavBar.tsx
@@ -9,6 +9,197 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {ImageLoader} from "@movit/utils";
 
 const NavBar = () => {
+  const menus = [
+    {
+      title: 'products',
+      children: [
+        {
+          title: 'Commerce',
+          description: 'Sell on a website, point of sale, or social media',
+          children: [
+            {
+              title: 'Online store',
+            },
+            {
+              title: 'Point of sale',
+            },
+            {
+              title: 'Invoices',
+            },
+            {
+              title: 'Online checkout links',
+            },
+            {
+              title: 'Virtual terminal',
+            },
+            {
+              title: 'Social media',
+            },
+            {
+              title: 'All online solutions',
+            },
+            {
+              title: 'Payment processing',
+            },
+            {
+              title: 'Browse hardware',
+            },
+            {
+              title: 'All commerce solutions',
+            }
+          ]
+        },
+
+        {
+          title: 'Customers',
+          description: 'Drive business with marketing, messaging, and loyalty programs.',
+          children: [
+            {
+              title: 'Support',
+            },
+            {
+              title: 'What`s new',
+            },
+            {
+              title: 'Contact sales',
+            },
+            {
+              title: 'Seller Community',
+            },
+            {
+              title: 'Resource Center',
+            },
+          ]
+        },
+
+        {
+          title: 'Banking',
+          description: 'Take payments, access funds instantly, and get a loan.',
+          children: [
+            {
+              title: 'Online store',
+            },
+            {
+              title: 'Point of sale',
+            },
+            {
+              title: 'Invoices',
+            },
+            {
+              title: 'Online checkout links',
+            },
+            {
+              title: 'Virtual terminal',
+            },
+            {
+              title: 'Social media',
+            },
+            {
+              title: 'All online solutions',
+            },
+            {
+              title: 'Payment processing',
+            },
+            {
+              title: 'Browse hardware',
+            },
+            {
+              title: 'All commerce solutions',
+            }
+          ]
+        },
+
+        {
+          title: 'Staff',
+          description: 'Run your team efficiently with permissions, timecards, and payroll.',
+          children: [
+            {
+              title: 'Online store',
+            },
+            {
+              title: 'Point of sale',
+            },
+            {
+              title: 'Invoices',
+            },
+            {
+              title: 'Online checkout links',
+            },
+            {
+              title: 'Virtual terminal',
+            },
+            {
+              title: 'Social media',
+            },
+            {
+              title: 'All online solutions',
+            },
+            {
+              title: 'Payment processing',
+            },
+            {
+              title: 'Browse hardware',
+            },
+            {
+              title: 'All commerce solutions',
+            }
+          ]
+        },
+
+        {
+          title: 'Developer',
+          description: 'Connect third-party apps or build custom solutions',
+          children: [
+            {
+              title: 'Online store',
+            },
+            {
+              title: 'Point of sale',
+            },
+            {
+              title: 'Invoices',
+            },
+            {
+              title: 'Online checkout links',
+            },
+            {
+              title: 'Virtual terminal',
+            },
+            {
+              title: 'Social media',
+            },
+            {
+              title: 'All online solutions',
+            },
+            {
+              title: 'Payment processing',
+            },
+            {
+              title: 'Browse hardware',
+            },
+            {
+              title: 'All commerce solutions',
+            }
+          ]
+        },
+      ]
+    },
+
+    
+
+    {
+      title: 'Business Types',
+    },
+
+    {
+      title: 'The Movement',
+    },
+
+    {
+      title: 'Pricing',
+    },
+  ]
+
   return (
       <Navbar expand="md" className='shadow-sm py-3'>
           <Container>
@@ -17,11 +208,11 @@ const NavBar = () => {
               </Navbar.Brand>
               <Navbar.Toggle  />
               <Navbar.Collapse >
-                  <MenuDesktop/>
-                  <div style={{ width: '200px' , margin: 'auto'}} className="d-flex">
+                  <MenuDesktop menus={menus}/>
+                  <div style={{margin: 'auto'}} className="d-flex">
                       <Nav
-                          className="me-auto w-100 justify-content-center align-items-center align-items-xxl-baseline"
-                          style={{ maxHeight: '100px' }}
+                          className="me-auto justify-content-md-center align-items-md-center align-items-xxl-baseline"
+                          style={{ width: 200, maxHeight: '100px' }}
                           navbarScroll
                       >
                           <Nav.Link className='shopLog'  href="#action1">Log In</Nav.Link>
